8b787cdb5c17f4d5d4677078c255cbde48dd5c05
darraghkennedy <dkennedy121314@gmail.com>
2024-01-24 22:17:06 +0000

    Code challenge submission for question: 191 Number of 1 Bits

---
+++ 191 Number of 1 Bits/191number-of-1-bits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int bits = 0;
+        String num = Integer.toBinaryString(n);
+        for(int i = 0; i < num.length(); i++) {
+            if(num.charAt(i) == '1') {
+                bits ++;
+            }
+        }
+        return bits;
+    }
+}
